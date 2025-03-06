2c0190a8020e43b52db502952c2a9f270d246803
Kofo Idowu <idowukofo3@gmail.com>
2025-03-06 21:18:49 +0100

    Changes made to todoController.js, added PUT and DELETE Requests

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -2,45 +2,56 @@
 const todoList = require('../models/to_doModel');
 
 //Fetching all the items on the list from the database
-exports.getItems = async(req, res) => {
-    try{
+exports.getItems = async (req, res) => {
+    try {
         const items = await todoList.find();
         res.json(items);
     } catch (error) {
-        res.status(500).json({ message: "Error getting items on the list.", error});
+        res.status(500).json({ message: "Error getting items on the list.", error });
     }
 };
 
 //Fetching the items by Id from the database using the GET Request
-exports.getItemsbyId = async (req,res) => {
+exports.getItemsbyId = async (req, res) => {
     try {
         const item = await todoList.findById(req.params.id);
-        if(!item) return res.status(404).json({message: "Item not found"});
+        if (!item) return res.status(404).json({ message: "Item not found" });
         res.json(item);
     } catch (error) {
-        res.status(500).json({ message: "Error fetching item"});
+        res.status(500).json({ message: "Error fetching item", error });
     }
 };
 
 //Adding an item to the database using the POST method
-exports.addItem = async(req, res) => {
+exports.addItem = async (req, res) => {
     try {
-        const {Title, Description, Status, Timestamps} = req.body;
-        const newItem = new todoList({Title, Description, Status, Timestamps});
+        const { Title, Description, Status, Timestamps } = req.body;
+        const newItem = new todoList({ Title, Description, Status, Timestamps });
         await newItem.save();
-        res.status(201).json({ message: "Item added successfully"})
+        res.status(201).json({ message: "Item added successfully" })
     } catch (error) {
-        res.status(500).json({message: "Error adding an item"});
+        res.status(500).json({ message: "Error adding an item", error });
     }
 };
 
-// 
-exports.updateId = async(req, res) => {
+//Updating an item using PUT Method
+exports.updateId = async (req, res) => {
     try {
-        const updatedItem = await todoList.findByIdAndUpdate(req.params.id, req.body, { new: true});
-        if(!updatedItem) return res.status(404).json({ message: "Item not found"});
+        const updatedItem = await todoList.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        if (!updatedItem) return res.status(404).json({ message: "Item not found" });
         res.json(updatedItem);
     } catch (error) {
-        res.status(500).json({message : 'Error updating Item'})
+        res.status(500).json({ message: 'Error updating Item', error });
     }
-}
+};
+
+//Deleting information from the database using DELETE Request
+exports.deleteItem = async (req, res) => {
+    try {
+        const deletedItem = await todoList.findByIdAndDelete(req.params.id);
+        if (!deletedItem) return res.status(404).json({ message: "Item not found" });
+        res.json({ message: "Item deleted successfully" });
+    } catch (error) {
+        res.status(500).json({ message: "Error deleting item", error });
+    }
+};
