ca3fdcb8f0029201ca2cc7dfb555de128ca37b84
Kofo Idowu <idowukofo3@gmail.com>
2025-03-06 20:40:08 +0100

    Changes made to todoController, added GET and POST Requests

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -10,3 +10,26 @@ exports.getItems = async(req, res) => {
         res.status(500).json({ message: "Error getting items on the list.", error});
     }
 };
+
+//Fetching the items by Id from the database using the GET Request
+exports.getItemsbyId = async (req,res) => {
+    try {
+        const item = await todoList.findById(req.params.id);
+        if(!item) return res.status(404).json({message: "Item not found"});
+        res.json(item);
+    } catch (error) {
+        res.status(500).json({ message: "Error fetching item"});
+    }
+};
+
+//Adding an item to the database using the POST method
+exports.addItem = async(req, res) => {
+    try {
+        const {Title, Description, Status, Timestamps} = req.body;
+        const newItem = new todoList({Title, Description, Status, Timestamps});
+        await newItem.save();
+        res.status(201).json({ message: "Item successfully updated"})
+    } catch (error) {
+        res.status(500).json({message: "Error updating Item"});
+    }
+};
