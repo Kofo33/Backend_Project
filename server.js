97125fc7197cd3bf50640aeb2564138013e7d061
Kofo Idowu <idowukofo3@gmail.com>
2025-03-07 14:11:08 +0100

    Installed nodemon, made changes to server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const connectDB = require('./config/db');
 const todoRoutes = require('./routes/todoRoutes');
 
 //Connecting to MongoDB
-require('dotenv').config();
+require('dotenv').config(); // This loads environment variables (.process.env)
 connectDB();
 
 //Starting an Express App
@@ -14,4 +14,17 @@ app.use(express.json());
 //Loads database connection, routes and controllers
 app.use('/api/tasks', todoRoutes); //Uses the todo Routes
 
-app.listen(6700, () => console.log('Server is running successfully on Port 6700'));
+//Handling unknown routes
+app.use((req, res, next) => {
+    res.status(404).json({ error: "Route not found." })
+});
+
+//Global error handling
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ error: "Oops!, Something went wrong" });
+});
+
+//Starting the server
+const PORT = process.env.PORT || 6700;
+app.listen(PORT, () => console.log(`Server is running successfully on Port ${PORT}`));
