6f818aae94a195aca97603ceca4d265f1fb7ab4a
Kofo Idowu <idowukofo3@gmail.com>
2025-03-07 11:00:25 +0100

    Updated todoValidations.js and todoModels.js

--- models/to_doModel.js
+++ models/to_doModel.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 
 // Creating a new Schema for the database
 const todoSchema = new mongoose.Schema({
-    Title: String,
-    Description: String,
-    Status: {type: String, default: "Pending"},
+    title: String,
+    description: String,
+    status: {type: String, default: "Pending"},
 }, {timestamps: true}); // This adds createdAt and updatedAt field to the list
 
 // Creates a To-Do List model that lets me interact with the database
