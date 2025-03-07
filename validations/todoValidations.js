00a66fb77d025bc08f7f0370dc8a9e27ab2a8f51
Kofo Idowu <idowukofo3@gmail.com>
2025-03-07 14:38:50 +0100

    Changes made todoValidations.js

--- validations/todoValidations.js
+++ validations/todoValidations.js
@@ -5,7 +5,7 @@ const taskSchema = Joi.object({
     title: Joi.string().min(3).max(100).required(),
     description: Joi.string().optional().allow('').max(500), //.optional() and .allow() allow a field to be missing and also allows empty strings.
     dueDate: Joi.date().greater('now').optional(), // The due date of the task is going to be in the future
-    Ccmpleted: Joi.boolean().default(false).optional() // To choose whether it is completed or not and the default setting is not completed.
+    Completed: Joi.boolean().default(false).optional() // To choose whether it is completed or not and the default setting is not completed.
 });
 
 module.exports = taskSchema;
